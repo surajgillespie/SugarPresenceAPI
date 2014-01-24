27a751d41d10d259bb0aea4a28a6bd3af3a05cdd
Suraj K S <suraj_gillespie@yahoo.co.in>
2014-01-24 12:48:42 +0530

    Add callbacks to the functions

--- presence.js
+++ presence.js
@@ -1,32 +1,54 @@
-
- 
   var socket;
 
+  function SugarPresence() {
 
-  function joinNetwork(nickname, colour)
-  {
-        socket = new WebSocket('ws://localhost:8039');
-        console.log('Created socket');
-        return socket;     
   }
 
-  function leaveNetwork()
-  {
+  SugarPresence.prototype.joinNetwork = function (nickname, colour, callback) {
+
+      socket = new WebSocket('ws://localhost:8039');
+      console.log('Created socket');
+
+      socket.onerror = function (error) {
+          console.log('WebSocket Error: ' + error);
+      };
+
+      callback("userid");
+
+  }
+
+  SugarPresence.prototype.leaveNetwork = function () {
       socket.close();
-      console.log('Socket closed');  
+      console.log('Socket closed');
   }
 
-  function sendMessage(group_id, data)
-  {
-    console.log(data);
-    socket.send(data);
-  }  
-  
+  SugarPresence.prototype.onConnectionOpen = function (callback) {
+      socket.onopen = function (event) {
+          callback(event);
+      };
+  }
+
+  SugarPresence.prototype.onDataReceived = function (callback) {
+      socket.onmessage = function (event) {
+          callback(event);
+      };
+  }
+
+  SugarPresence.prototype.onConnectionClose = function (callback) {
+      socket.onclose = function (event) {
+          callback(event);
+      };
+  }
+
+  SugarPresence.prototype.sendMessage = function (group_id, data) {
+      console.log(data);
+      socket.send(data);
+  }
+
+  /*
   function listUsers()
   {
     //
     //*code 
     return ("Connected Users");
-  }
-
-
+  }*/
