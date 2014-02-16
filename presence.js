318131fbcfe97d90d12b320a2e5483ffcfbb9618
Suraj K S <suraj_gillespie@yahoo.co.in>
2014-02-16 16:06:57 +0530

    Run js-beautifier

--- presence.js
+++ presence.js
@@ -1,63 +1,61 @@
-  
- function SugarPresence() {
-    var socket;
+  function SugarPresence() {
+      var socket;
   }
 
-  SugarPresence.prototype.joinNetwork = function (userInfo) {
+  SugarPresence.prototype.joinNetwork = function(userInfo) {
 
       this.socket = new WebSocket('ws://localhost:8039');
       console.log('Created socket');
 
-      this.socket.onerror = function (error) {
+      this.socket.onerror = function(error) {
           console.log('WebSocket Error: ' + error);
       };
   }
 
-  SugarPresence.prototype.leaveNetwork = function () {
+  SugarPresence.prototype.leaveNetwork = function() {
       this.socket.close();
       console.log('Socket closed');
   }
 
-  SugarPresence.prototype.onConnectionOpen = function (callback) {
-      this.socket.onopen = function (event) {
+  SugarPresence.prototype.onConnectionOpen = function(callback) {
+      this.socket.onopen = function(event) {
           callback(event);
       };
   }
 
-  SugarPresence.prototype.onDataReceived = function (callback) {
+  SugarPresence.prototype.onDataReceived = function(callback) {
 
-      this.onMessageReceived(function(message)
-      {
-          if(message.type === 'message')
-          callback(message.data);
+      this.onMessageReceived(function(message) {
+          if (message.type === 'message')
+              callback(message.data);
       });
-        
-  }//for messages received from users
 
-  SugarPresence.prototype.onMessageReceived = function (callback) {
-      this.socket.onmessage = function (event) {
-                
-                var edata = event.data;
+  } //for messages received from users
 
-                try {
-                    var json = JSON.parse(edata);
-                } catch (e) {
-                    console.log('This doesn\'t look like a valid JSON: ', edata);
-                    return;
-                }
+  SugarPresence.prototype.onMessageReceived = function(callback) {
+      this.socket.onmessage = function(event) {
 
-                callback(json);
-      
+          var edata = event.data;
+
+          try {
+              var json = JSON.parse(edata);
+          } catch (e) {
+              console.log('This doesn\'t look like a valid JSON: ', edata);
+              return;
+          }
+
+          callback(json);
+
       };
-  }//will be called by functions to retrieve the message send from server
+  } //will be called by functions to retrieve the message send from server
 
-  SugarPresence.prototype.onConnectionClose = function (callback) {
-      this.socket.onclose = function (event) {
+  SugarPresence.prototype.onConnectionClose = function(callback) {
+      this.socket.onclose = function(event) {
           callback(event);
       };
   }
 
-  SugarPresence.prototype.sendMessage = function (group_id, mdata) {
+  SugarPresence.prototype.sendMessage = function(group_id, mdata) {
       console.log(mdata);
       var sjson = JSON.stringify({
           type: 'message',
@@ -67,15 +65,14 @@
   }
 
 
-  SugarPresence.prototype.listUsers = function (group_id, callback) {
+  SugarPresence.prototype.listUsers = function(group_id, callback) {
       var sjson = JSON.stringify({
           type: 'requestUserList'
       });
       this.socket.send(sjson);
 
-      this.onMessageReceived(function(message)
-      {
-          if(message.type === 'responseUserList')
-          callback(message.data);
+      this.onMessageReceived(function(message) {
+          if (message.type === 'responseUserList')
+              callback(message.data);
       });
   }
