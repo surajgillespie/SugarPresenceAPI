0c3ca8a941289c0d1171f6811078cd952f12211b
Suraj K S <suraj_gillespie@yahoo.co.in>
2014-02-16 16:00:06 +0530

    Make socket variable private and separate system and user messages

--- presence.js
+++ presence.js
@@ -1,38 +1,58 @@
-  var socket;
-
-  function SugarPresence() {
-
+  
+ function SugarPresence() {
+    var socket;
   }
 
   SugarPresence.prototype.joinNetwork = function (userInfo) {
 
-      socket = new WebSocket('ws://localhost:8039');
+      this.socket = new WebSocket('ws://localhost:8039');
       console.log('Created socket');
 
-      socket.onerror = function (error) {
+      this.socket.onerror = function (error) {
           console.log('WebSocket Error: ' + error);
       };
   }
 
   SugarPresence.prototype.leaveNetwork = function () {
-      socket.close();
+      this.socket.close();
       console.log('Socket closed');
   }
 
   SugarPresence.prototype.onConnectionOpen = function (callback) {
-      socket.onopen = function (event) {
+      this.socket.onopen = function (event) {
           callback(event);
       };
   }
 
   SugarPresence.prototype.onDataReceived = function (callback) {
-      socket.onmessage = function (event) {
-          callback(event);
+
+      this.onMessageReceived(function(message)
+      {
+          if(message.type === 'message')
+          callback(message.data);
+      });
+        
+  }//for messages received from users
+
+  SugarPresence.prototype.onMessageReceived = function (callback) {
+      this.socket.onmessage = function (event) {
+                
+                var edata = event.data;
+
+                try {
+                    var json = JSON.parse(edata);
+                } catch (e) {
+                    console.log('This doesn\'t look like a valid JSON: ', edata);
+                    return;
+                }
+
+                callback(json);
+      
       };
-  }
+  }//will be called by functions to retrieve the message send from server
 
   SugarPresence.prototype.onConnectionClose = function (callback) {
-      socket.onclose = function (event) {
+      this.socket.onclose = function (event) {
           callback(event);
       };
   }
@@ -43,13 +63,19 @@
           type: 'message',
           data: mdata
       });
-      socket.send(sjson);
+      this.socket.send(sjson);
   }
 
 
-  SugarPresence.prototype.listUsers = function () {
+  SugarPresence.prototype.listUsers = function (group_id, callback) {
       var sjson = JSON.stringify({
           type: 'requestUserList'
       });
-      socket.send(sjson);
+      this.socket.send(sjson);
+
+      this.onMessageReceived(function(message)
+      {
+          if(message.type === 'responseUserList')
+          callback(message.data);
+      });
   }
